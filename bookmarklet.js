49ef2c9a0b6889f55981d8a6cdc73abc1bd345cb
sverweij <481@nepserver.fake>
2014-12-19 14:30:25 +0100

    update title

--- bookmarklet.js
+++ bookmarklet.js
@@ -5,5 +5,5 @@ javascript:(function() {
     s = se(document);
     for (i=0; i<frames.length && !s; i++) s = se(frames[i].document);
     if (!s || s==='') s = prompt('Enter%20text%20to%20read','');
-    open('http://localhost:8666/rsvp.html' + (s ? '?play=1&text=' + encodeURIComponent(s) : '')).focus();
+    open('https://sverweij.github.io/wordywordy/index.html' + (s ? '?play=1&text=' + encodeURIComponent(s) : '')).focus();
 })();
