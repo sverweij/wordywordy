66e83e08a36c4dad344e1e4041680645bcca7dd6
sverweij <481@nepserver.fake>
2014-12-19 14:17:41 +0100

    initial commit

---
+++ bookmarklet.js
@@ -0,0 +1,9 @@
+javascript:(function() {
+    function se(d) {
+        return d.selection ? d.selection.createRange().text : d.getSelection()
+    }
+    s = se(document);
+    for (i=0; i<frames.length && !s; i++) s = se(frames[i].document);
+    if (!s || s==='') s = prompt('Enter%20text%20to%20read','');
+    open('http://localhost:8666/rsvp.html' + (s ? '?play=1&text=' + encodeURIComponent(s) : '')).focus();
+})();
