467b6023c032fd42f742a1a167e32f98b2fa2cda
sverweij <sverweij@users.noreply.github.com>
2015-01-09 20:03:32 +0100

    update GPL3 disclaimers

--- src/script/ui-control/controler.js
+++ src/script/ui-control/controler.js
@@ -633,16 +633,16 @@ define(["../chopper/chopper",
 /*
  This file is part of WordyWordy.
 
- mscgen_js is free software: you can redistribute it and/or modify
+ WordyWordy is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
 
- mscgen_js is distributed in the hope that it will be useful,
+ WordyWordy is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with mscgen_js.  If not, see <http://www.gnu.org/licenses/>.
+ along with WordyWordy.  If not, see <http://www.gnu.org/licenses/>.
 */ 
